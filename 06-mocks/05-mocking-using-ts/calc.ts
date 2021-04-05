e36d73997774fe2aac006deb670a78dbeeb16d9b
Rene Enriquez <rene.enriquez@clevertech.biz>
2021-04-05 12:59:41 -0500

    feat: remove console.log

--- 06-mocks/05-mocking-using-ts/calc.ts
+++ 06-mocks/05-mocking-using-ts/calc.ts
@@ -1,6 +1,9 @@
 import {add} from "./add";
 
 export const calculate = (operation: string, a: number, b: number): void => {
-    if (operation === 'Add')
-        console.log('result has been', add(a, b))
+    if (operation === 'Add'){
+        add(a, b)
+        // un-comment if u wanna play with this
+        // console.log('result has been', add(a, b))
+    }
 }
